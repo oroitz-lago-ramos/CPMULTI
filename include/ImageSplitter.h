86956895f54f9026bdeee5fdbcbc9c954440072e
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2025-02-05 17:36:02 +0100

    feat : added a merge image function

--- include/ImageSplitter.h
+++ include/ImageSplitter.h
@@ -8,6 +8,9 @@ class ImageSplitter {
 public:
     // Méthode statique pour diviser l'image en quatre parties
     static std::vector<cv::Mat> splitImage(const cv::Mat& inputImage);
+
+    // Order is tL, tR, bL, bR
+    static cv::Mat mergeImage(const std::vector<cv::Mat>& subImages);
 };
 
 #endif // IMAGE_SPLITTER_H
